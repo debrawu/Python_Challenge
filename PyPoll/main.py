aa2b74627d4427a1c440b08708429fe419483cc0
debrawu <dwu1124@gmail.com>
2020-08-20 17:31:44 -0500

    changed the print function, and tried to fix errors..

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,6 +24,7 @@ with open(PyPollData) as csvfile:
         #set the columns to the correct list - votes = integer value of row 0, candidate = row 2
         count_votes += 1
         Potential_Candidates = row[2]
+        count_votes = len(TotalVotes)
    
         #start a loop to find the different candidates and their voter count
         if Potential_Candidates in AllCandidates:
@@ -33,30 +34,43 @@ with open(PyPollData) as csvfile:
         else:
             AllCandidates.append(Potential_Candidates)
             TotalVotes.append(1)
+    
 
-#find percentages
-percentages = []
-most_votes = TotalVotes[0]
-most_votes_index = 0
+        print(f'Total Votes: {count_votes}')
 
-#create loop for percentages for each candidate
-for i in range(len(AllCandidates)):
-    voter_percentage = round(TotalVotes[i]/count_votes)*100,2
-    percentages.append(voter_percentage)
+# #find percentages
+# percentages = []
+# most_votes = TotalVotes[0]
+# most_votes_index = 0
 
-    if TotalVotes[i] > most_votes:
-        most_votes = TotalVotes[i]
-        most_votes_index = i
+# #create loop for percentages for each candidate
+# for i in range(len(AllCandidates)):
+#     voter_percentage = round((TotalVotes[i]/count_votes)*100)
+#     percentages.append(voter_percentage)
 
-winner = AllCandidates[most_votes_index]
+#     if TotalVotes[i] > most_votes:
+#         most_votes = TotalVotes[i]
+#         most_votes_index = i
 
-# #results
-print("Election Results")
-print("--------------------")
-print("Total Votes: {count_votes}")
-print("--------------------")
-for i in range (len(AllCandidates)):
-    print(f"{AllCandidates[i]}: {percentages[i]}% {TotalVotes[i]}")
-print("--------------------")
-print(f"Winner: {str(winner)}")
-print("--------------------")
+# winner = AllCandidates[most_votes_index]
+
+# # #results
+# Election_Results = (f'Election Results\n'
+#                     f'---------------------\n'
+#                     f'Total Votes: int(count_votes)\n'
+#                     f'---------------------\n'
+#                     for i in range(len(AllCandidates)):
+#                         f'{AllCandidates[i]}: {percentages[i]}.000% ({TotalVotes[i]})\n'
+#                     f'---------------------\n'
+#                     f'Winner: {str(winner)}\n'
+#                     f'---------------------')
+
+# print("Election Results")
+# print("--------------------")
+# print("Total Votes: (count_votes)")
+# print("--------------------")
+# for i in range (len(AllCandidates)):
+#     print(f"{AllCandidates[i]}: {percentages[i]}.000% ({TotalVotes[i]})")
+# print("--------------------")
+# print(f"Winner: {str(winner)}")
+# print("--------------------")
